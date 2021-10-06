f1dc01e3852d368c74c6547100eb4eb2ec6835b2
harryrosadi <harry.harry.rosadi@gmail.com>
2021-10-06 20:09:58 +0700

    first commit delivair

---
+++ src/main/java/com/delivair/DelivairApplication.java
@@ -0,0 +1,13 @@
+package com.delivair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivair/DelivairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
