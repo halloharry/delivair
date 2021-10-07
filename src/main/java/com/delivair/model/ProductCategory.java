3aeafe6aaa2ff45ee45d46ac5a34df901401d481
cahya93 <ekocahyanto007@gmail.com>
2021-10-07 12:56:53 +0700

    create Product Category

--- src/main/java/com/delivair/model/ProductCategory.java
+++ src/main/java/com/delivair/model/ProductCategory.java
@@ -1,2 +1,19 @@
-package com.delivair.model;public class ProductCategory {
+package com.delivair.model;
+
+import com.delivair.common.auditable.ModelBase;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@Setter
+@Getter
+public class ProductCategory extends ModelBase {
+
+    @Column
+    private String name;
 }
