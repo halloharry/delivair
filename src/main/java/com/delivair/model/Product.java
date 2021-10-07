f7b0b14f3c4d7d6c638ca7f482e15de3e2c4f0bf
cahya93 <ekocahyanto007@gmail.com>
2021-10-07 12:54:22 +0700

    create model Product

--- src/main/java/com/delivair/model/Product.java
+++ src/main/java/com/delivair/model/Product.java
@@ -1,4 +1,39 @@
 package com.delivair.model;
 
-public class Product {
+import com.delivair.common.auditable.ModelBase;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@Setter
+@Getter
+public class Product extends ModelBase {
+    @Column
+    private String name;
+
+    @Column
+    private String brand;
+
+    @Column
+    private int amount;
+
+    @Column
+    private String code;
+
+    @Column
+    private int size;
+
+    @Column
+    private int price;
+
+    @Column
+    private String image;
+
+    @Column
+    private boolean is_ready;
 }
