82b7f7161552cddff3de11471baa97a7cd26e595
cahya93 <ekocahyanto007@gmail.com>
2021-10-07 13:57:53 +0700

    create payment controller

---
+++ src/main/java/com/delivair/module/payment/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.delivair.module.payment.controller;
+
+import com.delivair.model.Payment;
+import com.delivair.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class PaymentController {
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    // get all payment
+    @GetMapping("/getAllPayment")
+    public List<Payment> getAllPayment(){
+        return paymentRepository.findAll();
+    }
+    
+    @PostMapping("/payment")
+    public Payment createPayment(@RequestBody Payment payment){
+        return paymentRepository.save(payment);
+    }
+
+}
---
+++ src/main/java/com/delivair/repository/PaymentRepository.java
@@ -0,0 +1,2 @@
+package com.delivair.repository;public interface PaymentRepository {
+}
