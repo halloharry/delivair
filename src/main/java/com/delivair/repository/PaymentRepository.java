9e9b7407388cdab0d4b741ca3c0ff1c448e1821f
cahya93 <ekocahyanto007@gmail.com>
2021-10-07 13:58:27 +0700

    create paymentRepository

--- src/main/java/com/delivair/repository/PaymentRepository.java
+++ src/main/java/com/delivair/repository/PaymentRepository.java
@@ -1,2 +1,9 @@
-package com.delivair.repository;public interface PaymentRepository {
+package com.delivair.repository;
+
+import com.delivair.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
