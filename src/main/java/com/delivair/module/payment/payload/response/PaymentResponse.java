24f19a4d4bef2f1b7f560a90b2213683d972d8aa
cahya93 <ekocahyanto007@gmail.com>
2021-10-08 22:12:40 +0700

    create payment response

--- src/main/java/com/delivair/module/payment/payload/response/PaymentResponse.java
+++ src/main/java/com/delivair/module/payment/payload/response/PaymentResponse.java
@@ -1,2 +1,12 @@
-package com.delivair.module.payment.payload.response;public class PaymentResponse {
+package com.delivair.module.payment.payload.response;
+
+import lombok.Data;
+
+@Data
+public class PaymentResponse {
+    private String id;
+    private String name;
+    private String totalPayment;
+    private String noResi;
+    private String status;
 }
